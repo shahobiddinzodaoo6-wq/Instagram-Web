9dde8f99f3240e31df20fe26fce6b6aa09f46612
Firdavszoda <amirfirdavszoda@gmail.com>
2026-04-22 15:53:25 +0500

    changes commit

--- src/app/(auth)/accounts/login/page.tsx
+++ src/app/(auth)/accounts/login/page.tsx
@@ -4,35 +4,33 @@ import { axiosRequest, saveToken } from "./token"
 
 const Page = () => {
     const router = useRouter()
-    const handleSubmit = async(event) =>
-        {   
-            event.preventDefault()
-            const obj = 
-            {
-                userName:(event.target["inpName"].value),
-                password:(event.target["inpPassword"].value)
-            }
-            try {
-                const {data} = await axiosRequest.post(`/Account/Login`,obj)
-                if(data.statusCode == 200)
-                    {
-                        saveToken(data.data)
-                        router.push("/")
-                    }
-            } catch (error) {
-                console.error(error);
-                
-            }
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        const obj =
+        {
+            userName: (event.target["inpName"].value),
+            password: (event.target["inpPassword"].value)
         }
-  return (
-    <div>
-        <form onSubmit={handleSubmit}>
-            <input type="text" name="inpName" placeholder="Name" />
-            <input type="text" name="inpPassword" placeholder="Password" />
-            <button type="submit">Save</button>
-        </form>
-    </div>
-  )
+        try {
+            const { data } = await axiosRequest.post(`/Account/Login`, obj)
+            if (data.statusCode == 200) {
+                saveToken(data.data)
+                router.push("/")
+            }
+        } catch (error) {
+            console.error(error);
+
+        }
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="inpName" placeholder="Name" />
+                <input type="text" name="inpPassword" placeholder="Password" />
+                <button type="submit">Save</button>
+            </form>
+        </div>
+    )
 }
 
 export default Page
